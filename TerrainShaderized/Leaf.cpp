9ae1610639b71c74ba4b77963e4820a5071e8061
Thomas Walsh (walsht) <walsht@coventry.ac.uk>
2019-11-24 22:09:41 +0000

    tried to make random leaves

--- TerrainShaderized/Leaf.cpp
+++ TerrainShaderized/Leaf.cpp
@@ -1,33 +1,35 @@
 #include "Leaf.h"
 
-
-
-
 Leaf::Leaf(glm::vec3 base1)
 {
-	this->base1 = base1;
-	this->base2 = base1 + glm::vec3(1.0f, 1.0f, 0.0f);
-	this->base3 = base1 + glm::vec3(-1.0f, 1.0f, 0.0f);
-	this->base4 = base1 + glm::vec3(0.0f, 2.0f, 0.0f);
+	//srand(time(NULL));
+	float angle = random(-1.0f, 1.0f);
+	float radi = random(0.3f, 0.6f);
+	float height = random(0.2f, 0.6f);
 
-	this->LeafVertices[0].coords[0] = base1.x;
-	this->LeafVertices[0].coords[1] = base1.y;
-	this->LeafVertices[0].coords[2] = base1.z;
+	this->base1 = base1;
+	this->base2 = base1 + glm::vec3(radi, height, angle);
+	this->base3 = base1 + glm::vec3(-radi, height, angle);
+	this->base4 = base1 + glm::vec3(0.0f, height * 2, angle * 2);
+
+	this->LeafVertices[0].coords[0] = this->base1.x;
+	this->LeafVertices[0].coords[1] = this->base1.y;
+	this->LeafVertices[0].coords[2] = this->base1.z;
 	this->LeafVertices[0].coords[3] = 1.0f;
 
-	this->LeafVertices[1].coords[0] = base2.x;
-	this->LeafVertices[1].coords[1] = base2.y + 1.0f;
-	this->LeafVertices[1].coords[2] = base2.z;
+	this->LeafVertices[1].coords[0] = this->base2.x;
+	this->LeafVertices[1].coords[1] = this->base2.y;
+	this->LeafVertices[1].coords[2] = this->base2.z;
 	this->LeafVertices[1].coords[3] = 1.0f;
 
-	this->LeafVertices[2].coords[0] = base3.x - 1.0f;
-	this->LeafVertices[2].coords[1] = base3.y + 1.0f;
-	this->LeafVertices[2].coords[2] = base3.z;
+	this->LeafVertices[2].coords[0] = this->base3.x;
+	this->LeafVertices[2].coords[1] = this->base3.y;
+	this->LeafVertices[2].coords[2] = this->base3.z;
 	this->LeafVertices[2].coords[3] = 1.0f;
 
-	this->LeafVertices[3].coords[0] = base4.x;
-	this->LeafVertices[3].coords[1] = base4.y + 2.0f;
-	this->LeafVertices[3].coords[2] = base4.z;
+	this->LeafVertices[3].coords[0] = this->base4.x;
+	this->LeafVertices[3].coords[1] = this->base4.y;
+	this->LeafVertices[3].coords[2] = this->base4.z;
 	this->LeafVertices[3].coords[3] = 1.0f;
 
 }
